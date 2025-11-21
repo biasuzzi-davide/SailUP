cb5297ea0b6ef9bf32daedbc835d8321511fcc30
NotHoss <hossnv@gmail.com>
2025-11-21 19:22:00 +0100

    implementat il  metodo per la registrazione di un nuovo utente nel database

--- includes/auth/auth.php
+++ includes/auth/auth.php
@@ -5,10 +5,34 @@ require_once __DIR__ . '/../../config/db_connect.php';
 
 /**
  * registrazione nuovo utente
- * @return true
+ * @return bool
  */
-function registerUser(){
-    
+function registerUser($nome,$cognome,$cf,$email,$password,$idIndirizzo,$patente=null){
+    global $conn;
+    $hashedPassword=password_hash($password,PASSWORD_DEFAULT);
+    //insert nel database
+    $sql= "INSERT INTO Utente
+            (Nome, Cognome, CF, Email, PasswordHash, Numero_Patente_Nautica, IDIndirizzo)
+            VALUES (:nome, :cognome, :cf, :email, :hashedPassword,)";
+
+    $stmt= $conn->prepare($sql);
+
+    try {
+        return $stmt->execute([
+            ":nome"        => $nome,
+            ":cognome"     => $cognome,
+            ":cf"          => $cf,
+            ":email"       => $email,
+            ":pass"        => $hashedPassword,
+            ":patente"     => $patente,
+            ":idIndirizzo" => $idIndirizzo
+        ]);
+    } catch (PDOException $e) {
+         
+        return false;
+    }
+
+
 }
 
 /**
