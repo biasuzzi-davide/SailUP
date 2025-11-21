81318ca216faae875c53997a2f360f3b7a947d60
NotHoss <hossnv@gmail.com>
2025-11-21 13:32:21 +0100

    iniziato file per login, registrazioni ecc

---
+++ includes/auth/auth.php
@@ -0,0 +1,22 @@
+<?php
+require_once __DIR__ . '/../session/session.php';
+require_once __DIR__ . '/../../config/db_connect.php';
+
+
+/**
+ * registrazione nuovo utente
+ * @return true
+ */
+function registerUser(){
+    
+}
+
+/**
+ * login utente
+ * @return bool
+ */
+function login(){
+    //rendo visbile la variabile conn presente nel file db_connect (config)
+    global $conn;
+
+}
