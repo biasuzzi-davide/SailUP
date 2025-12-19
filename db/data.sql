bd7c5aa2b81be961521ce07418644a90bdb05f2b
Davide Biasuzzi <biasuzzi.davide@gmail.com>
2025-12-19 18:09:29 +0100

    cambio tipologia experience in tipologia prodotto sulla tabella prodotti

--- db/data.sql
+++ db/data.sql
@@ -18,12 +18,9 @@ INSERT INTO Utente (Nome, Cognome, CF, Email, PasswordHash, Numero_Patente_Nauti
 ('Giulia', 'Bianchi', 'BNCGLI95M55H501Z', 'giulia@email.com', 'hashed_secret_giulia', NULL, 3, 0);
 
 -- 3. INSERIMENTO PRODOTTI
-INSERT INTO Prodotto (IDProdotto, Tipo_Prodotto, Tipologia_Experience, Nome_Prodotto, Descrizione_Breve, Prezzo_Base, Posti_Totali, Accessibile_Disabili, Lunghezza_Barca_Metri, Richiede_Patente) VALUES
--- Prodotto 1: Noleggio (Barca) -> Tipologia_Experience deve essere NULL
-('BARCA-GOZZO-01', 'Noleggio', NULL, 'Gozzo Sorrentino Classico', 'Ideale per giornate di relax.', 250.00, 6, 0, 7.50, 0),
--- Prodotto 2: Noleggio (Gommone potente) -> Tipologia_Experience deve essere NULL
-('GOMMONE-SPORT-05', 'Noleggio', NULL, 'Gommone Sport 200cv', 'Velocità e divertimento.', 400.00, 8, 0, 8.20, 1),
--- Prodotto 3: Experience -> Tipologia_Experience deve essere COMPILATO
+INSERT INTO Prodotto (IDProdotto, Tipo_Prodotto, Tipologia_Prodotto, Nome_Prodotto, Descrizione_Breve, Prezzo_Base, Posti_Totali, Accessibile_Disabili, Lunghezza_Barca_Metri, Richiede_Patente) VALUES
+('BARCA-GOZZO-01', 'Noleggio', 'Gozzo', 'Gozzo Sorrentino Classico', 'Ideale per giornate di relax.', 250.00, 6, 0, 7.50, 0),
+('GOMMONE-SPORT-05', 'Noleggio', 'Gommone', 'Gommone Sport 200cv', 'Velocità e divertimento.', 400.00, 8, 0, 8.20, 1),
 ('EXP-TRAMONTO-01', 'Experience', 'Romantico', 'Aperitivo al Tramonto', 'Tour della costa con prosecco.', 50.00, 10, 1, NULL, NULL);
 
 -- 4. PRODOTTO EXTRA (Upselling)
@@ -47,10 +44,10 @@ INSERT INTO Articolo_Blog (IDAutore, Titolo, Descrizione_Breve, Contenuto, Data_
 
 -- 7. MEDIA (Foto)
 INSERT INTO Media (URL_Media, Testo_Alternativo, Tipo_Media, IDUtente, IDProdotto, IDArticolo) VALUES
-('/uploads/users/avatar_mario.jpg', 'Avatar Admin', 'Immagine', 1, NULL, NULL),
-('/uploads/products/gozzo_main.jpg', 'Foto Gozzo in mare', 'Immagine', NULL, 'BARCA-GOZZO-01', NULL),
-('/uploads/products/sunset_exp.jpg', 'Coppia al tramonto', 'Immagine', NULL, 'EXP-TRAMONTO-01', NULL),
-('/uploads/blog/cale_nascoste.jpg', 'Cala splendida', 'Immagine', NULL, NULL, 1);
+('../img/Azimut_55_fly_2.webp', 'Avatar Admin', 'Immagine', 1, NULL, NULL),
+('../img/Azimut_55_fly_2.webp', 'Foto Gozzo in mare', 'Immagine', NULL, 'BARCA-GOZZO-01', NULL),
+('../img/Azimut_55_fly_2.webp', 'Coppia al tramonto', 'Immagine', NULL, 'EXP-TRAMONTO-01', NULL),
+('../img/Azimut_55_fly_2.webp', 'Cala splendida', 'Immagine', NULL, NULL, 1);
 
 -- 8. PRENOTAZIONI
 INSERT INTO Prenotazione (IDUtente, IDProdotto, Data_Ora_Inizio, Data_Ora_Fine, Prezzo_Totale, Stato_Prenotazione) VALUES
