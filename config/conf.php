bb09431c9ba646b15995bbcf0f1e11b224b5ec16
marcon-effe <framar514@gmail.com>
2026-01-10 15:45:17 +0100

    reso il logo link in modo condizionale e risolto il margin richiesto da davide ieri

--- config/conf.php
+++ config/conf.php
@@ -1,7 +1,7 @@
 <?php
 // File contenente le credenziali del database (Membro del gruppo: Davide Biasuzzi)
 define('DB_HOST', 'localhost');
-define('DB_USER', 'dbiasuzz'); 
-define('DB_PASS', 'Mee8EpahWee5ahth');
-define('DB_NAME', 'dbiasuzz');
+define('DB_USER', 'frmarcon'); 
+define('DB_PASS', 'cheiriPey5eev0oc');
+define('DB_NAME', 'frmarcon');
 ?>
