ecf3a13cdb8defb78a1bf39d4c5ee36103c6e836
marcon-effe <framar514@gmail.com>
2026-01-14 09:26:29 +0100

    tolto js da php, non so il motivo

--- public/php/profilo.php
+++ public/php/profilo.php
@@ -58,25 +58,5 @@ $html = str_replace('[ADMIN_MENU_ITEM]', buildAdminMenuItem(), $html);
 $html = str_replace('[ADMIN_BREADCRUMB]', buildAdminBreadcrumb(), $html);
 $html = str_replace(array_keys($placeholders + $addrPlaceholders), array_values($placeholders + $addrPlaceholders), $html);
 
-// Forza i valori delle statistiche dopo il load per evitare sovrascritture client-side
-$syncScript = '<script>(function(){'
-    . 'const tot=' . json_encode($statTotPren) . ';'
-    . 'const att=' . json_encode($statAttive) . ';'
-    . 'const apply=function(){'
-        . 'const t=document.querySelector(\"[data-stat=tot]\");'
-        . 'const a=document.querySelector(\"[data-stat=attive]\");'
-        . 'if(t) t.textContent=tot;'
-        . 'if(a) a.textContent=att;'
-    . '};'
-    . 'document.addEventListener(\"DOMContentLoaded\",function(){'
-        . 'apply();'
-        . 'const targets=document.querySelectorAll(\"[data-stat=tot],[data-stat=attive]\");'
-        . 'const obs=new MutationObserver(apply);'
-        . 'targets.forEach(el=>obs.observe(el,{childList:true,subtree:true,characterData:true}));'
-        . 'setTimeout(()=>obs.disconnect(),5000);'
-    . '});'
-    . '})();</script>';
-$html = str_replace('</body>', $syncScript . '</body>', $html);
-
 echo $html;
 ?>
